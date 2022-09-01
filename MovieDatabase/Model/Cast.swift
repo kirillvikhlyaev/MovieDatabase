be96e75f021a49ee7a9714f1d25e97d39c85aeca
Mark Mikhailov <pprimak@mail.ru>
2022-09-01 12:47:01 +0400

    Update Cast, MovieDetails

--- MovieDatabase/Model/Cast.swift
+++ MovieDatabase/Model/Cast.swift
@@ -8,18 +8,40 @@
 import Foundation
 
 struct CastResponse: Codable {
-    var cast: [Cast]
+    let id: Int
+    let cast, crew: [Cast]
 }
 
-struct Cast: Codable, Identifiable {
-    var id: Int?
-    var name: String?
-    var character: String?
-    var profile_path: String?
+// MARK: - Cast
+struct Cast: Codable {
+    let adult: Bool
+    let gender, id: Int
+    let knownForDepartment, name, originalName: String
+    let popularity: Double
+    let profilePath: String?
+    let castID: Int?
+    let character: String?
+    let creditID: String
+    let order: Int?
+    let department, job: String?
     var profilePhoto: String {
-        if let path = profile_path {
-            return "https://image.tmdb.org/t/p/original/\(path)"
-        }
-        return "https://picsum.photos/200/300"
+            if let path = profilePath {
+                return "https://image.tmdb.org/t/p/original/\(path)"
+            }
+            return "https://picsum.photos/200/300"
     }
+
+    enum CodingKeys: String, CodingKey {
+        case adult, gender, id
+        case knownForDepartment = "known_for_department"
+        case name
+        case originalName = "original_name"
+        case popularity
+        case profilePath = "profile_path"
+        case castID = "cast_id"
+        case character
+        case creditID = "credit_id"
+        case order, department, job
+    }
+
 }
